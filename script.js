353296454b092def0c783771e3911d99bd09031f
dinoburoh <dinoburoh@gmail.com>
2024-01-06 04:33:56 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const saveButton = document.getElementById("input-btn")
+const inputText = document.getElementById("input-el")
+const ulText = document.getElementById("ul-el")
+let textValues = []
+
+saveButton.addEventListener("click", function () {
+    textValues.push(inputText.value)
+    // ulText.textContent += inputText.value + " "
+    const li = document.createElement("li")
+    ulText.append(li)   
+    li.innerHTML = `
+        <a href='${inputText.value}' target='_blank'>
+        ${inputText.value}
+        </a>
+    `
+    inputText.value = " "
+})
+
