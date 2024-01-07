de758093da51b27a04677cb8bdadd2a8636c3d21
dinoburoh <dinoburoh@gmail.com>
2024-01-07 14:21:42 +0530

    Second commit

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const inputText = document.getElementById("input-el")
 const ulText = document.getElementById("ul-el")
 let textValues = []
 
+localStorage.setItem("name", "DINO")
+localStorage.clear()
+
 saveButton.addEventListener("click", function () {
     textValues.push(inputText.value)
     // ulText.textContent += inputText.value + " "
